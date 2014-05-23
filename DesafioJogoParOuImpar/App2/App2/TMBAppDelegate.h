992993eee4fcbeb31aa25f7a4ae49c4fc139e867
Thiago <thiagobernardes10@gmail.com>
2014-05-23 14:05:47 -0300

    Initial Commit

---
+++ DesafioJogoParOuImpar/App2/App2/TMBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TMBAppDelegate.h
+//  App2
+//
+//  Created by Thiago Bernardes on 5/23/14.
+//  Copyright (c) 2014 BEPiD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
