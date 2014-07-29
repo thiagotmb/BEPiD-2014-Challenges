f3fc3cca11640a738188b6e3d193394bbbacbce4
Thiago Bernardes <thiagobernardes10@gmail.com>
2014-07-29 14:41:53 -0300

    Initial Commit

---
+++ Desafio_SalaD_110_Thiago_Meira_Bernardes_Versionamento/ControllingSource2/ControllingSource2Tests/ControllingSource2Tests.m
@@ -0,0 +1,34 @@
+//
+//  ControllingSource2Tests.m
+//  ControllingSource2Tests
+//
+//  Created by Thiago Bernardes on 7/29/14.
+//  Copyright (c) 2014 TMB. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ControllingSource2Tests : XCTestCase
+
+@end
+
+@implementation ControllingSource2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
